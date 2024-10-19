dbac2c2db9db27a8f7a92ae34b60c698b1597a9f
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2024-10-19 23:57:19 +0900

    finish: packages, variables and functions

--- boiler-plate/cmd/hello/main.go
+++ boiler-plate/cmd/hello/main.go
@@ -5,12 +5,20 @@ import (
 	// "math/rand"
 )
 
+const foo = "foo!!"
+
 func hoge(x, y int) (z, a int) {
 	z = x + y
 	a = x - y
 	return
 }
 
+var c, python bool = true, true
+
 func main() {
-	fmt.Println(hoge(2, 3))
+	// i := 2
+	// j := int64(i)
+	// fmt.Println(hoge(2, 3))
+	// fmt.Println(i, j, c, python)
+	fmt.Println(foo)
 }
